da31b74aeef241b8e9e6c0eabb78971d2777447f
gengxf <15619280131@163.com>
2018-09-01 17:18:14 +0800

    first commit

--- FMShield.ts
+++ FMShield.ts
@@ -235,7 +235,7 @@ namespace FMShield {
          */
     //% weight=30
     //% blockGap=8
-    //% blockId=motor_motorRun block="Motor run|%m|speed|%speed"
+    //% blockId=motor_motorRun block="Motor run|%m|速度|%speed"
     //% speed.min=-100 speed.max=100
     //% shim=FMShield::motorRun
     export function motor_motorRun(m: FMMotorSN, speed: number): void {
